025b3a3727f9a300a9fb2f425bd487589dc43409
mojtaba <mojtaba@DESKTOP-57M28DK>
2025-09-13 00:06:34 +0330

    add mem to Relation

--- analysis.h
+++ analysis.h
@@ -42,113 +42,94 @@ inline std::vector<Relation> find_constant_relations(
     int trials = 5
 ) {
     std::vector<Relation> out;
-    std::set<std::string> seen_relations;
-
-    for (int r : Simulator::TRACKED_REGS) {
-        bool first = true;
-        int64_t expected_delta = 0;
-        bool stable = true;
-
-        for (int t = 0; t < trials; t++) {
-            auto init = Simulator::make_random_regs();
-            RegMap final;
-            sim.emulate(code, init, final);
-
-            int64_t diff = (int64_t)final[r] - (int64_t)init[r];
-            if (first) {
-                expected_delta = diff;
-                first = false;
-            }
-            else if (expected_delta != diff) {
-                stable = false;
-                break;
-            }
-        }
-
-        if (!first && stable && expected_delta != 0) {
-            out.push_back({
-                Simulator::reg_name(r),
-                "0x0",
-                expected_delta,
-                true
-                });
-        }
-    }
-
-
-    for (int r1 : Simulator::TRACKED_REGS) {
-        for (int r2 : Simulator::TRACKED_REGS) {
-            if (r1 == r2) continue;
-
-            bool first = true;
-            int64_t expected_delta = 0;
-            bool stable = true;
-
-            for (int t = 0; t < trials; t++) {
-                auto init = Simulator::make_random_regs();
-                RegMap final;
-                sim.emulate(code, init, final);
-
-                int64_t diff = (int64_t)final[r1] - (int64_t)final[r2];
-                if (first) {
-                    expected_delta = diff;
-                    first = false;
-                }
-                else if (expected_delta != diff) {
-                    stable = false;
-                    break;
-                }
-            }
-
-            if (!first && stable) {
-                out.push_back({
-                    Simulator::reg_name(r1),
-                    Simulator::reg_name(r2),
-                    expected_delta,
-                    true
-                    });
-            }
-        }
-    }
-
-
-
+    std::vector<RegMap> finals(trials);
+    std::vector<std::vector<MemAccessModel>> mems(trials);
 
     for (int t = 0; t < trials; t++) {
         auto init = Simulator::make_random_regs();
         RegMap final;
         sim.emulate(code, init, final);
+        finals[t] = final;
 
-        for (auto& m : sim.mem_accesses) {
-            if (m.reg_src != -1) {
-                int64_t diff = (int64_t)m.value - (int64_t)final[m.reg_src];
+        std::vector<MemAccessModel> mem_copy;
+        for (auto& m : sim.mem_accesses)
+            mem_copy.push_back({ m.is_write, m.addr, m.value, m.reg_src });
+        mems[t] = mem_copy;
+    }
 
-   
-                int64_t rsp_offset = (int64_t)m.addr - (int64_t)final[UC_X86_REG_RSP];
-                std::string lhs;
-                if (rsp_offset >= 0 && rsp_offset < 0x1000) {
-                    lhs = "mem[RSP + 0x" + to_hex(rsp_offset) + "]";
-                }
-                else {
-                    lhs = "mem[0x" + to_hex(m.addr) + "]";
-                }
+    for (int r : Simulator::TRACKED_REGS) {
+        bool stable = true;
+        int64_t delta0 = (int64_t)finals[0][r] - (int64_t)finals[0][r];
+        for (int t = 1; t < trials; t++) {
+            int64_t delta = (int64_t)finals[t][r] - (int64_t)finals[t][r];
+            if (delta != delta0) {
+                stable = false;
+                break;
+            }
+        }
+        if (stable && delta0 != 0)
+            out.push_back({ Simulator::reg_name(r), "0x0", delta0, true });
+    }
 
-                std::string key = lhs + "=" + Simulator::reg_name(m.reg_src) + "+" + std::to_string(diff);
-                if (seen_relations.find(key) == seen_relations.end()) {
-                    out.push_back({
-                        lhs,
-                        Simulator::reg_name(m.reg_src),
-                        diff,
-                        true
-                        });
-                    seen_relations.insert(key);
+    for (int r1 : Simulator::TRACKED_REGS) {
+        for (int r2 : Simulator::TRACKED_REGS) {
+            if (r1 == r2) continue;
+            bool stable = true;
+            int64_t delta0 = (int64_t)finals[0][r1] - (int64_t)finals[0][r2];
+            for (int t = 1; t < trials; t++) {
+                int64_t delta = (int64_t)finals[t][r1] - (int64_t)finals[t][r2];
+                if (delta != delta0) {
+                    stable = false;
+                    break;
                 }
+            }
+            if (stable)
+                out.push_back({ Simulator::reg_name(r1), Simulator::reg_name(r2), delta0, true });
+        }
+    }
+
+    for (size_t i = 0; i < mems[0].size(); i++) {
+        auto& m0 = mems[0][i];
+        if (m0.reg_src == -1) continue;
+        bool stable = true;
+        int64_t delta0 = (int64_t)m0.value - (int64_t)finals[0][m0.reg_src];
+        for (int t = 1; t < trials; t++) {
+            auto it = std::find_if(mems[t].begin(), mems[t].end(), [&](const MemAccessModel& x) { return x.addr == m0.addr; });
+            if (it == mems[t].end()) { stable = false; break; }
+            int64_t delta = (int64_t)it->value - (int64_t)finals[t][it->reg_src];
+            if (delta != delta0) { stable = false; break; }
+        }
+        if (stable) {
+            int64_t rsp_offset = (int64_t)m0.addr - (int64_t)finals[0][UC_X86_REG_RSP];
+            std::string lhs = (rsp_offset >= 0 && rsp_offset < 0x1000) ? "mem[RSP + 0x" + to_hex(rsp_offset) + "]" : "mem[0x" + to_hex(m0.addr) + "]";
+            out.push_back({ lhs, Simulator::reg_name(m0.reg_src), delta0, true });
+        }
+    }
+
+    for (size_t i = 0; i < mems[0].size(); i++) {
+        for (size_t j = i + 1; j < mems[0].size(); j++) {
+            auto& m1 = mems[0][i];
+            auto& m2 = mems[0][j];
+            bool stable = true;
+            int64_t delta0 = (int64_t)m1.value - (int64_t)m2.value;
+            for (int t = 1; t < trials; t++) {
+                auto it1 = std::find_if(mems[t].begin(), mems[t].end(), [&](const MemAccessModel& x) { return x.addr == m1.addr; });
+                auto it2 = std::find_if(mems[t].begin(), mems[t].end(), [&](const MemAccessModel& x) { return x.addr == m2.addr; });
+                if (it1 == mems[t].end() || it2 == mems[t].end()) { stable = false; break; }
+                int64_t delta = (int64_t)it1->value - (int64_t)it2->value;
+                if (delta != delta0) { stable = false; break; }
+            }
+            if (stable) {
+                std::string lhs = "mem[0x" + to_hex(m1.addr) + "]";
+                std::string rhs = "mem[0x" + to_hex(m2.addr) + "]";
+                out.push_back({ lhs, rhs, delta0, true });
             }
         }
     }
 
     return out;
 }
+
 
 
 
